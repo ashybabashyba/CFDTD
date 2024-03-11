a3634a9fb2a8a7d53511c1c1dda97021cc07ba0d
Carlos J. Ramos <cramoss@unal.edu.co>
2024-03-11 11:47:16 +0100

    Cambio de sintaxis

--- DesfaseTest.py
+++ DesfaseTest.py
@@ -7,9 +7,9 @@ from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 ke = 120
+
 ex1 = np.zeros(ke)
 hy1 = np.zeros(ke)
-
 ex2 = np.zeros(ke)
 hy2 = np.zeros(ke)
 
@@ -31,6 +31,11 @@ t0 = 40
 spread = 10
 nsteps = 10000
 
+probeE1 = np.zeros((ke, nsteps+1))
+probeH1 = np.zeros((ke, nsteps+1))
+probeE2 = np.zeros((ke, nsteps+1))
+probeH2 = np.zeros((ke, nsteps+1))
+
 # Spatial and time steps
 epsilon0=1
 mu0=1
@@ -38,6 +43,44 @@ c0=1/np.sqrt(epsilon0*mu0)
 deltax=1
 deltat=deltax/c0*cfl
 cb=deltat*c0/deltax
+
+# Main CFDTD Loop
+for time_step in range(1, nsteps + 1):
+
+    # Calculate the Ex field
+    for k in range(1, ke):
+        if k<np.floor(kp):
+            ex1[k] = ex1[k] + cb * (hy1[k - 1] - hy1[k])
+        elif k==np.floor(kp):
+            ex1[k] = ex1[k] + leftover*cb * (hy1[k - 1] - hy1[k])
+
+        ex2[k] = ex2[k] + cb * (hy2[k - 1] - hy2[k])
+
+
+    # Put a Gaussian pulse in the middle
+    ex1[kc] += deltat*exp(-0.5 * ((t0 - deltat*time_step) / spread) ** 2)
+    hy1[kc] += deltat*exp(-0.5 * ((t0 - deltat/2 - deltax/2/c0 - deltat*time_step) / spread) ** 2)
+
+    ex2[kc] += deltat*exp(-0.5 * ((t0 - deltat*time_step) / spread) ** 2)
+    hy2[kc] += deltat*exp(-0.5 * ((t0 - deltat/2 - deltax/2/c0 - deltat*time_step) / spread) ** 2)
+
+
+    # Calculate the Hy field
+    for k in range(1, ke):
+        if k==np.floor(kp):
+            hy1[k] = hy1[k] + leftover*cb * (ex1[k])
+        elif k<np.floor(kp):
+            hy1[k] = hy1[k] + cb * (ex1[k] - ex1[k + 1])
+
+    for k in range(ke - 1):
+        hy2[k] = hy2[k] + cb * (ex2[k] - ex2[k + 1])
+
+
+    probeE1[:,time_step]=ex1[:]
+    probeH1[:,time_step]=hy1[:]
+    probeE2[:,time_step]=ex2[:]
+    probeH2[:,time_step]=hy2[:]
+    
 
 
 # Create a figure and axis for the animation
@@ -62,53 +105,15 @@ def init():
     plt.subplots_adjust(bottom=0.2, hspace=0.45)
     return line1, line2, line3, line4
 
-# Function to update the plot in each animation frame
-def update(frame):
-    global ex1, hy1, ex2, hy2
-
-    # Calculate the Ex field
-    for k in range(1, ke):
-        if k<np.floor(kp):
-            ex1[k] = ex1[k] + cb * (hy1[k - 1] - hy1[k])
-        elif k==np.floor(kp):
-            ex1[k] = ex1[k] + leftover*cb * (hy1[k - 1] - hy1[k])
-
-        ex2[k] = ex2[k] + cb * (hy2[k - 1] - hy2[k])
-
-
-    # Put a Gaussian pulse in the middle
-    ex1[kc] += deltat*exp(-0.5 * ((t0 - deltat*frame) / spread) ** 2)
-    hy1[kc] += deltat*exp(-0.5 * ((t0 - deltat/2 - deltax/2/c0 - deltat*frame) / spread) ** 2)
-
-    ex2[kc] += deltat*exp(-0.5 * ((t0 - deltat*frame) / spread) ** 2)
-    hy2[kc] += deltat*exp(-0.5 * ((t0 - deltat/2 - deltax/2/c0 - deltat*frame) / spread) ** 2)
-
-
-    # Calculate the Hy field
-    for k in range(1, ke):
-        if k==np.floor(kp):
-            hy1[k] = hy1[k] + leftover*cb * (ex1[k])
-        elif k<np.floor(kp):
-            hy1[k] = hy1[k] + cb * (ex1[k] - ex1[k + 1])
-
-    for k in range(ke - 1):
-        hy2[k] = hy2[k] + cb * (ex2[k] - ex2[k + 1])
-
-
-    # Update the plot data
-    line1.set_ydata(ex1)
-    line2.set_ydata(hy1)
-    line3.set_ydata(ex2)
-    line4.set_ydata(hy2)
-    
-    # Display the time step in the plot
-    ax1.text(100, 0.5, 'T = {}'.format(frame), horizontalalignment='center')
-
-
+def animate(i):
+    line1.set_ydata(probeE1[:,i])
+    line2.set_ydata(probeH1[:,i])
+    line3.set_ydata(probeE2[:,i])
+    line4.set_ydata(probeH2[:,i])
     return line1, line2, line3, line4
 
 # Create the animation
-ani = FuncAnimation(fig, update, frames=nsteps, init_func=init, blit=True, interval=2, repeat=False)
+ani = FuncAnimation(fig, animate, frames=nsteps, init_func=init, blit=True, interval=2, repeat=False)
 
 # Show the animation
 plt.show()
