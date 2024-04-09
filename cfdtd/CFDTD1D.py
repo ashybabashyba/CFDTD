a1f94d21a314d007d0f0d9e2beedca883dbba391
Carlos J. Ramos <cramoss@unal.edu.co>
2024-04-09 18:29:18 +0200

    Fixed visual EM pulse test

--- cfdtd/CFDTD1D.py
+++ cfdtd/CFDTD1D.py
@@ -9,7 +9,7 @@ class Mesh():
                 
         V1 = np.linspace(0, kpi, int(1 + kpi/dx))
         V2 = np.array([box_size])
-        if kpi == box_size:
+        if np.isclose(kpi, box_size):
             self.vx = V1
         else:
             self.vx = np.concatenate((V1, V2))
@@ -22,7 +22,7 @@ class Mesh():
     
     def getLeftover(self):
         kp = self.getLength()
-        if self.kp==np.floor(kp):
+        if kp==np.floor(kp):
             return 1.0
         else:
             return 1.0 /(kp-np.floor(kp))
@@ -30,22 +30,39 @@ class Mesh():
     def numberOfCells(self):
         return len(self.vx) - 1
 
-
-class CFDTD1D():
-    def __init__(self, mesh, cfl, t0, spread):
-        
-        self.mesh = mesh
-        
-        self.cfl = cfl
-        self.t0 = t0
+class InitialPulse():
+    def __init__(self, initial_time, spread):
+        self.t0 = initial_time
         self.spread = spread
 
-        self.dt = self.mesh.getDX() * self.cfl
-        self.cb = self.dt/self.mesh.getDX()
+    def getT0(self):
+        return self.t0
+    
+    def getSpread(self):
+        return self.spread
+
+    def gaussianPulse(self, dx, dt, step):
+        E0 = dt*exp(-0.5 * ((self.t0 - dt*step) / self.spread) ** 2)
+        H0 = dt*exp(-0.5 * ((self.t0 - dt/2 - dx/2 - dt*step) / self.spread) ** 2)
+        return E0, H0
+
+class CFDTD1D():
+    def __init__(self, mesh, initialPulse, cfl):
+        
+        self.mesh = mesh
+        self.pulse = initialPulse
+        
+        self.cfl = cfl
+        self.t0 = self.pulse.getT0()
+        self.spread = self.pulse.getSpread()
+
+        self.dx = self.mesh.getDX()
+        self.dt = self.dx * self.cfl
+        self.cb = self.dt/self.dx
     
     def buildFields(self):
-        ex = np.zeros(self.mesh.numberOfCells())
-        hy = np.zeros(self.mesh.numberOfCells())
+        ex = np.zeros(self.mesh.numberOfCells()+1)
+        hy = np.zeros(self.mesh.numberOfCells()+1)
 
         return ex, hy
     
@@ -56,35 +73,33 @@ class CFDTD1D():
 
         return probeE, probeH
     
-    def ElectricGaussianPulse(self, time_step):
-        return self.dt*exp(-0.5 * ((self.t0 - self.dt*time_step) / self.spread) ** 2)
-    
-    def MagneticGaussianPulse(self, time_step):
-        return self.dt*exp(-0.5 * ((self.t0 - self.dt/2 - self.dx/2 - self.dt*time_step) / self.spread) ** 2)
-    
     def run(self, nsteps):
         ex, hy = self.buildFields()
         probeE, probeH = self.buildFieldsInAllTimeSteps(nsteps)
         
-        kc = int(self.ke / 5)
+        kp = self.mesh.getLength()
+        kc = int(kp / 5)
+        self.ke = int(kp)
 
         leftover = self.mesh.getLeftover()
 
         for time_step in range(nsteps):
 
-            for k in range(1, self.numberOfCells()):
-                if k<np.floor(self.kp):
+            for k in range(1, self.mesh.numberOfCells()):
+                if k<np.floor(kp):
                     ex[k] = ex[k] + self.cb * (hy[k - 1] - hy[k])
-                elif k==np.floor(self.kp):
+                elif k==np.floor(kp):
                     ex[k] = ex[k] + leftover * self.cb * (hy[k - 1] - hy[k])
 
-            ex[kc] += self.ElectricGaussianPulse(time_step)
-            hy[kc] += self.MagneticGaussianPulse(time_step)
+            Wave = self.pulse.gaussianPulse(self.dx, self.dt, time_step)
+            ex[kc] += Wave[0]
+            hy[kc] += Wave[1]
+ 
 
             for k in range(self.ke - 1):
-                if k==np.floor(self.kp):
+                if k==np.floor(kp):
                     hy[k] = hy[k] + leftover * self.cb * (ex[k])
-                elif k<np.floor(self.kp):
+                elif k<np.floor(kp):
                     hy[k] = hy[k] + self.cb * (ex[k] - ex[k + 1])
 
             probeE[:,time_step]=ex[:]
@@ -92,45 +107,3 @@ class CFDTD1D():
             
         return probeE, probeH
 
-
-
-## Here below is the comprobation of obtaining the same result as before ## 
-
-# fdtd = CFDTD1D_Class(ke=200, kp=100.5, cfl=0.75, t0=40, spread=10, nsteps=1000)
-# FinalFields = fdtd.CFDTDLoop()
-# probeE = FinalFields[0]
-# probeH = FinalFields[1]
-
-
-# # Create a figure and axis for the animation
-# fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6))
-# line1, = ax1.plot(probeE[:,0], color='k', linewidth=1)
-# line2, = ax2.plot(probeH[:,0], color='k', linewidth=1)
-# ax1.axvline(x=100.5, color='r', linestyle='--', label='kp')
-# ax2.axvline(x=100.5, color='r', linestyle='--', label='kp')
-
-# # Function to initialize the plot
-# def init():
-#     ax1.set_ylabel('E$_x$', fontsize='14')
-#     ax1.set_xlim(0, 200)
-#     ax1.set_ylim(-2.2, 2.2)
-
-#     ax2.set_ylabel('H$_y$', fontsize='14')
-#     ax2.set_xlabel('FDTD cells')
-#     ax2.set_xlim(0, 200)
-#     ax2.set_ylim(-2.2, 2.2)
-#     plt.subplots_adjust(bottom=0.2, hspace=0.45)
-#     return line1, line2
-
-
-# def animate(i):
-#     line1.set_ydata(probeE[:,i])
-#     line2.set_ydata(probeH[:,i])
-#     return line1, line2
-
-
-# # Create the animation
-# ani = FuncAnimation(fig, animate, frames=1001, init_func=init, blit=True, interval=2, repeat=False)
-
-# # Show the animation
-# plt.show()
--- test/test_1D.py
+++ test/test_1D.py
@@ -9,17 +9,42 @@ PEC_sheet = 119.5
 cfl = 0.5
 
 def test_pec_pulse():
-    mesh = Mesh(10.0, 0.1)
-    solver = CFDTD1D(mesh, 1.0, 40, 10)
-    probeE, probeH = solver.run(1000)
+    mesh = Mesh(100.0, 1.0)
+    pulse = InitialPulse(40, 10)
+    solver = CFDTD1D(mesh, pulse, 1.0)
+    probeE, probeH = solver.run(500)
 
-    for n in range(probeE.shape[1]):
-        plt.plot(mesh.vx, probeE[:,n])
-        plt.pause(0.01)
-        plt.grid(which='both')
-        plt.ylim(-1.1, 1.1)
-        plt.cla()
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6))
+    line1, = ax1.plot(probeE[:,0], color='k', linewidth=1)
+    line2, = ax2.plot(probeH[:,0], color='k', linewidth=1)
+    ax1.axvline(x=100.5, color='r', linestyle='--', label='kp')
+    ax2.axvline(x=100.5, color='r', linestyle='--', label='kp')
 
+    # Function to initialize the plot
+    def init():
+        ax1.set_ylabel('E$_x$', fontsize='14')
+        ax1.set_xlim(0, 200)
+        ax1.set_ylim(-2.2, 2.2)
+
+        ax2.set_ylabel('H$_y$', fontsize='14')
+        ax2.set_xlabel('FDTD cells')
+        ax2.set_xlim(0, 200)
+        ax2.set_ylim(-2.2, 2.2)
+        plt.subplots_adjust(bottom=0.2, hspace=0.45)
+        return line1, line2
+
+
+    def animate(i):
+        line1.set_ydata(probeE[:,i])
+        line2.set_ydata(probeH[:,i])
+        return line1, line2
+
+
+    # Create the animation
+    ani = FuncAnimation(fig, animate, frames=1001, init_func=init, blit=True, interval=2, repeat=False)
+
+    # Show the animation
+    plt.show()
 
 def test_mesh_comparative():
     conformal_mesh = cfdtd.SpatialMesh()
