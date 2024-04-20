7fecebc0fedbcad5ab74f0e03a962f312a6ac4d1
Carlos J. Ramos <cramoss@unal.edu.co>
2024-04-20 16:40:41 +0200

    Changed the visual test

--- test/test_1D.py
+++ test/test_1D.py
@@ -7,17 +7,18 @@ from cfdtd.CFDTD1D import *
 
 def test_visual_pec_pulse():
     mesh = Mesh(box_size=200.0, pec_sheet_position=199.5, dx=1.0)
-    pulse = InitialPulse(initial_time=40, initial_position=50, spread=10, pulse_type="Gaussian")
+    pulse = InitialPulse(initial_time=40, initial_position=100, spread=10, pulse_type="Gaussian")
     solver = CFDTD1D(mesh, pulse, boundary_type="periodic", cfl=0.5)
-    probeE, probeH = solver.run(1000)
+    probeE, probeH = solver.run(600)
 
     for n in range(probeE.shape[1]):
-        if n % 15 == 0:  # Skipping rate 
+        if n % 10 == 0:  # Skipping rate 
             plt.plot(mesh.xE, probeE[:,n], '.-', label='Electric Field')
             plt.plot(mesh.xH, probeH[:,n], '.-', label='Magnetic Field')
             plt.axvline(x=mesh.getPECSheetPosition(), color='r', linestyle='--', label='PEC sheet position')
             plt.legend()
             plt.ylim(-2.1, 2.1)
+            plt.title(n*solver.dt)
             plt.grid(which='both')
             plt.pause(0.01)  
             plt.cla()
