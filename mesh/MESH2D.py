c0ea7443ca67aaee3ca7786c697c37ed8fd0c71b
Carlos J. Ramos <cramoss@unal.edu.co>
2024-05-03 11:38:02 +0200

    Fixed the definition of the grid generator for the electric field.

--- mesh/MESH2D.py
+++ mesh/MESH2D.py
@@ -16,7 +16,7 @@ def inverseLineEquation(x0, x1):
         return lambda x: m_inv*(x - x0[1]) + x0[0]
 
 class Mesh():
-    def __init__(self, box_size, dx, dy, external_nodes_list_PEC='None'):
+    def __init__(self, box_size, dx, dy, external_nodes_list_PEC=None):
         self.dx = dx
         self.dy = dy
         self.boxSize = box_size
@@ -29,38 +29,36 @@ class Mesh():
 
         self.nodesList = external_nodes_list_PEC
     
-    def gridCreation(self):
-        if self.nodesList is None:
-            self.finalGridE = np.meshgrid(self.gridEx, self.gridEy)
-            self.finalGridH = np.meshgrid(self.gridHx, self.gridHy)
-        else:
-            self.columns = []
-            self.rows = []
+    def electricFieldGridCreation(self):
+        self.columns = []
+        self.rows = []
 
-            for i in self.gridEx:
-                self.columns.append(self.gridEy.tolist())
+        for i in self.gridEx:
+            self.columns.append(self.gridEy.tolist())
 
-            for i in self.gridEy:
-                self.rows.append(self.gridEx.tolist())
+        for i in self.gridEy:
+            self.rows.append(self.gridEx.tolist())
+
+        if self.nodesList is not None:
 
             for i in range(len(self.nodesList)):
                 current_node = self.nodesList[i]
                 next_node = self.nodesList[(i+1) % len(self.nodesList)]
 
                 for j in range(len(self.gridEx)):
-                    if min(current_node[0], next_node[0]) <= j <= max(current_node[0], next_node[0]):
-                        self.columns[j].append(lineEquation(current_node, next_node)(j))
+                    if min(current_node[0], next_node[0]) <= self.gridEx[j] <= max(current_node[0], next_node[0]):
+                        self.columns[j].append(lineEquation(current_node, next_node)(self.gridEx[j]))
                         self.columns[j].sort()
 
                 for k in range(len(self.gridEy)):
-                    if min(current_node[1], next_node[1]) <= k <= max(current_node[1], next_node[1]):
-                        self.rows[k].append(inverseLineEquation(current_node, next_node)(k))
+                    if min(current_node[1], next_node[1]) <= self.gridEy[k] <= max(current_node[1], next_node[1]):
+                        self.rows[k].append(inverseLineEquation(current_node, next_node)(self.gridEy[k]))
                         self.rows[k].sort()
 
-            return self.columns, self.rows
+        return self.columns, self.rows
 
-    def plot(self):
-        columns, rows = self.gridCreation()
+    def plotElectricFieldGrid(self):
+        columns, rows = self.electricFieldGridCreation()
         fig, ax = plt.subplots()
         for i, vec in enumerate(columns):
             x_coords = [self.gridEx[i]] * len(vec)  # Coordenadas x iguales para cada vector
@@ -72,13 +70,14 @@ class Mesh():
             x_coords = vec                      # Valores del vector como coordenadas x
             ax.plot(x_coords, y_coords, marker='s', color='b')
 
-        for k in range(len(self.nodesList)):
-            current_node = self.nodesList[k]
-            next_node = self.nodesList[(k+1) % len(self.nodesList)]
+        if self.nodesList is not None:
+            for k in range(len(self.nodesList)):
+                current_node = self.nodesList[k]
+                next_node = self.nodesList[(k+1) % len(self.nodesList)]
 
-            x_vals = np.linspace(current_node[0], next_node[0], 100)
-            y_vals = np.linspace(current_node[1], next_node[1], 100)
-            ax.plot(x_vals, y_vals, color='r')
+                x_vals = np.linspace(current_node[0], next_node[0], 100)
+                y_vals = np.linspace(current_node[1], next_node[1], 100)
+                ax.plot(x_vals, y_vals, color='r')
 
         ax.set_xlabel('Grid Ex')
         ax.set_ylabel('Grid Ey')
@@ -87,13 +86,7 @@ class Mesh():
         ax.grid(True)
         plt.show()
 
-    def getGridE(self):
-        return np.meshgrid(self.gridEx, self.gridEy)
-    
-    def getGridH(self):
-        return np.meshgrid(self.gridHx, self.gridHy)
 
-
-node_list = [(1,2), (2,3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 7), (8, 6), (9, 5)]
-mesh = Mesh(box_size=10, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list)
-mesh.plot()
+node_list = [(0,0), (1,10)]
+mesh = Mesh(box_size=10, dx=1.0, dy=0.5, external_nodes_list_PEC=node_list)
+mesh.plotElectricFieldGrid()
