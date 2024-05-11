5fd3efed08439f23623ba9885d9023a88a3da9d2
Carlos J. Ramos <cramoss@unal.edu.co>
2024-05-11 20:40:25 +0200

    Added 2 basic tests for PEC lines

---
+++ test/test_2D.py
@@ -0,0 +1,24 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import shapely as shape
+
+from mesh.MESH2D import *
+
+def test_visualization_of_the_mesh():
+    node_list = [(0,0), (10,1), (7, 8), (5, 10), (3, 4)]
+    mesh = Mesh(box_size=10, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list)
+    mesh.plotElectricFieldGrid()
+
+def test_number_of_intersections_of_a_line():
+    node_list = [(0,0), (10, 1)]
+    mesh = Mesh(box_size=10, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list)
+    #mesh.plotElectricFieldGrid()
+    for i in range(10):
+        assert np.isclose(mesh.getNumberOfIntersections((i,0)), 2)
+
+def test_conformal_cells_with_a_PEC_line():
+    node_list = [(0,0), (10,1)]
+    mesh = Mesh(box_size=10, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list)
+    # mesh.plotElectricFieldGrid()
+    conformalCells, nonConformalCells = mesh.getCellSepartionByType()
+    assert 10 == len(conformalCells)
