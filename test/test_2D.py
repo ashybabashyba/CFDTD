45d07e9db16ef6f3f153575a9430910730d9fd10
Carlos J. Ramos <cramoss@unal.edu.co>
2024-05-18 16:06:32 +0200

    Added general test to areas

--- test/test_2D.py
+++ test/test_2D.py
@@ -12,7 +12,7 @@ def test_visualization_of_the_mesh():
 def test_number_of_intersections_of_a_line():
     node_list = [(0,0), (10, 1)]
     mesh = Mesh(box_size=10, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list)
-    conformalCells, nonConformalCells = mesh.getCellSepartionByType()
+    conformalCells, outsideNonConformalCells, insideNonConformalCells = mesh.getCellSeparationByType()
     #mesh.plotElectricFieldGrid()
     for i in conformalCells:
         assert np.isclose(mesh.getNumberOfIntersections(i), 2)
@@ -21,12 +21,27 @@ def test_number_of_cells():
     node_list = [(0,0.5), (10,0.5)]
     mesh = Mesh(box_size=10, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list)
     # mesh.plotElectricFieldGrid()
-    conformalCells, nonConformalCells = mesh.getCellSepartionByType()
-    assert len(conformalCells) + len(nonConformalCells) == 100/mesh.dx/mesh.dy
+    conformalCells, outsideNonConformalCells, insideNonConformalCells = mesh.getCellSeparationByType()
+    assert len(conformalCells) + len(outsideNonConformalCells) + len(insideNonConformalCells) == 100/mesh.dx/mesh.dy
 
 def test_conformal_cells_with_a_PEC_line():
     node_list = [(0,0.5), (10,0.5)]
     mesh = Mesh(box_size=10, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list)
     # mesh.plotElectricFieldGrid()
-    conformalCells, nonConformalCells = mesh.getCellSepartionByType()
+    conformalCells, outsideNonConformalCells, insideNonConformalCells = mesh.getCellSeparationByType()
     assert 10/mesh.dx == len(conformalCells)
+
+def test_general_area():
+    node_list = [(0,0), (10,1), (7, 8), (5, 10), (3, 4)]
+    mesh = Mesh(box_size=10, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list)
+    # mesh.plotElectricFieldGrid()
+    conformalCells, outsideNonConformalCells, insideNonConformalCells = mesh.getCellSeparationByType()
+
+    for i in conformalCells:
+        assert 0 <= mesh.getCellArea(i) < mesh.dx * mesh.dy
+
+    for j in outsideNonConformalCells:
+        assert np.isclose(mesh.getCellArea(j), mesh.dx * mesh.dy)
+
+    for k in insideNonConformalCells:
+        assert np.isclose(mesh.getCellArea(k), 0)
