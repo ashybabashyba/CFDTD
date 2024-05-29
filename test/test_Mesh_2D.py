4ef7362a1e6ad844e912b98adfb0114d5cf9a61b
Carlos J. Ramos <cramoss@unal.edu.co>
2024-05-29 16:29:17 +0200

    Fixed tests

--- test/test_Mesh_2D.py
+++ test/test_Mesh_2D.py
@@ -15,28 +15,28 @@ def test_number_of_intersections_of_a_line():
     conformalCells, nonConformalCells = mesh.getCellSeparationByType()
     #mesh.plotElectricFieldGrid()
     for i in conformalCells:
-        assert np.isclose(mesh.getNumberOfIntersections(i), 2)
+        assert mesh.getNumberOfIntersections(i) == 2
 
 def test_number_of_cells_polygon():
     node_list = [(0.5,0.5), (9.5,0.5), (9.5, 9.5), (0.5, 9.5)]
     mesh = Mesh(box_size=10, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list)
     # mesh.plotElectricFieldGrid()
     conformalCells, outsideNonConformalCells, insideNonConformalCells = mesh.getCellSeparationByType()
-    assert len(conformalCells) + len(outsideNonConformalCells) + len(insideNonConformalCells) == mesh.boxSize**2/mesh.dx/mesh.dy
+    assert np.isclose(len(conformalCells) + len(outsideNonConformalCells) + len(insideNonConformalCells), mesh.boxSize**2/mesh.dx/mesh.dy)
 
 def test_number_of_cells_line():
     node_list = [(0,0.5), (10,0.5)]
     mesh = Mesh(box_size=10.0, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list)
     # mesh.plotElectricFieldGrid()
     conformalCells, nonConformalCells = mesh.getCellSeparationByType()
-    assert len(conformalCells) + len(nonConformalCells)  == mesh.boxSize**2/mesh.dx/mesh.dy
+    assert np.isclose(len(conformalCells) + len(nonConformalCells), mesh.boxSize**2/mesh.dx/mesh.dy)
 
 def test_conformal_cells_with_a_PEC_line():
     node_list = [(0,0.5), (10,0.5)]
     mesh = Mesh(box_size=10, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list)
     # mesh.plotElectricFieldGrid()
     conformalCells, nonConformalCells = mesh.getCellSeparationByType()
-    assert mesh.boxSize/mesh.dx == len(conformalCells)
+    assert np.isclose(mesh.boxSize/mesh.dx, len(conformalCells))
 
 def test_general_area_polygon():
     node_list = [(0.5,0.5), (9.5,0.5), (9.5, 9.5), (0.5, 9.5)]
