064442af28b96f8a11e4335ce1b971cc0ba386f2
Carlos J. Ramos <cramoss@unal.edu.co>
2024-05-30 12:53:26 +0200

    Added new argument in the mesh class to solve inside

--- test/test_2D.py
+++ test/test_2D.py
@@ -6,11 +6,11 @@ from mesh.MESH2D import *
 from cfdtd.CFDTD2D import *
 
 def test_visual_animation():
-    node_list = [(85.5,10.5), (95.5,10.5), (95.5, 95.5), (85.5, 95.5)]
+    node_list = [(25.5,10.5), (75.5,10.5), (75.5, 95.5), (25.5, 95.5)]
     initial_cell = (50, 50)
-    mesh = Mesh(box_size=100.0, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list, initial_wave_cell=initial_cell)
-    # mesh.plotElectricFieldGrid()
-    solver = CFDTD2D(mesh, initial_position=initial_cell, spread=1.0, cfl=0.99)
+    mesh = Mesh(box_size=100.0, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list, initial_wave_cell=initial_cell, complement=True)
+    mesh.plotElectricFieldGrid()
+    solver = CFDTD2D(mesh, initial_position=initial_cell, spread=5.0, cfl=0.5)
     nsteps = int(100 / solver.dt)
     probeEx, probeEy, probeHz, probeTime = solver.run(nsteps)
 
