52651001ab877965737184243521f0f2510390cb
Carlos J. Ramos <cramoss@unal.edu.co>
2024-06-07 21:27:43 +0200

    Changed the animation

--- test/test_2D.py
+++ test/test_2D.py
@@ -18,12 +18,12 @@ def test_visual_animation():
 
     solver.plotMagneticFieldAnimation(nsteps)
 
-def test_rectangular_waveguide10():
-    node_list = [(25.5,10.5), (75.5,10.5), (75.5, 95.5), (25.5, 95.5)]
+def test_rectangular_resonant_cavity11():
+    node_list = [(10.5,10.5), (95.5,10.5), (95.5, 95.5), (10.5, 95.5)]
     initial_cell = (50, 50)
     mesh = Mesh(box_size=100.0, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list, initial_wave_cell=initial_cell)
     # mesh.plotElectricFieldGrid()
-    pulse = InitialPulse(mesh=mesh, initial_position=initial_cell, spread=10.0, pulse_type='Rectangular Waveguide 10')
+    pulse = InitialPulse(mesh=mesh, initial_position=initial_cell, spread=10.0, pulse_type='Rectangular Resonant Cavity 11')
     solver = CFDTD2D(mesh, initialPulse=pulse, cfl=0.5)
     nsteps = int(100 / solver.dt)
     probeEx, probeEy, probeHz, probeTime = solver.run(nsteps)
