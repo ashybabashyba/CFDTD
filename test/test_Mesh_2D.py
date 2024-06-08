3dd4ddf8866d7f947dc347da2cdfa5fa0cd29a73
Carlos J. Ramos <cramoss@unal.edu.co>
2024-06-08 17:41:23 +0200

    Added new tests on the length depending on solving outside or inside, added more precisely test to verify that the sums of the lengths inside and outside are always dx or dy

--- test/test_Mesh_2D.py
+++ test/test_Mesh_2D.py
@@ -65,9 +65,9 @@ def test_general_area_line():
     for j in nonConformalCells:
         assert np.isclose(mesh.getCellArea(j), mesh.dx * mesh.dy)
 
-def test_cell_lengths_polygon():
-    node_list = [(0.5,0.5), (1.5,0.5), (1.5, 9.5), (0.5, 9.5)]
-    mesh = Mesh(box_size=10, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list, initial_wave_cell= (0, 9))
+def test_cell_lengths_polygon_outsidePEC():
+    node_list = [(0.5,0.5), (2.5,0.5), (2.5, 9.5), (0.5, 9.5)]
+    mesh = Mesh(box_size=10, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list, initial_wave_cell= (4, 9))
     # mesh.plotElectricFieldGrid()
     conformalCells, outsideNonConformalCells, insideNonConformalCells = mesh.getCellSeparationByType()
 
@@ -77,6 +77,33 @@ def test_cell_lengths_polygon():
 
         assert mesh.getCellLengths(i)["top"] == mesh.dx
         assert mesh.getCellLengths(i)["bottom"] == mesh.dx
+
+    for i in insideNonConformalCells:
+        assert mesh.getCellLengths(i)["left"] == 0
+        assert mesh.getCellLengths(i)["right"] == 0
+
+        assert mesh.getCellLengths(i)["top"] == 0
+        assert mesh.getCellLengths(i)["bottom"] == 0
+
+    for i in conformalCells:
+        assert mesh.getCellLengths(i)["left"] <= mesh.dy
+        assert mesh.getCellLengths(i)["right"] <= mesh.dy
+
+        assert mesh.getCellLengths(i)["top"] <= mesh.dx
+        assert mesh.getCellLengths(i)["bottom"] <= mesh.dx
+
+def test_cell_lengths_polygon_insidePEC():
+    node_list = [(0.5,0.5), (2.5,0.5), (2.5, 9.5), (0.5, 9.5)]
+    mesh = Mesh(box_size=10, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list, initial_wave_cell= (1, 1))
+    # mesh.plotElectricFieldGrid()
+    conformalCells, outsideNonConformalCells, insideNonConformalCells = mesh.getCellSeparationByType()
+
+    for i in outsideNonConformalCells:
+        assert mesh.getCellLengths(i)["left"] == 0
+        assert mesh.getCellLengths(i)["right"] == 0
+
+        assert mesh.getCellLengths(i)["top"] == 0
+        assert mesh.getCellLengths(i)["bottom"] == 0
 
     for i in insideNonConformalCells:
         assert mesh.getCellLengths(i)["left"] == mesh.dy
@@ -91,6 +118,37 @@ def test_cell_lengths_polygon():
 
         assert mesh.getCellLengths(i)["top"] <= mesh.dx
         assert mesh.getCellLengths(i)["bottom"] <= mesh.dx
+
+def test_cell_lengths_polygon_sumOutsideAndInside():
+    node_list = [(0.8,0.6), (2.5,0.6), (2.5, 9.5), (0.8, 9.5)]
+    mesh1 = Mesh(box_size=10, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list, initial_wave_cell= (1, 1))
+    # mesh1.plotElectricFieldGrid()
+    conformalCells, outsideNonConformalCells, insideNonConformalCells = mesh1.getCellSeparationByType()
+
+    mesh2 = Mesh(box_size=10, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list, initial_wave_cell= (5, 5))
+    # mesh2.plotElectricFieldGrid()
+    conformalCells, outsideNonConformalCells, insideNonConformalCells = mesh2.getCellSeparationByType()
+
+    for i in conformalCells:
+        assert np.isclose(mesh1.getCellLengths(i)["left"] + mesh2.getCellLengths(i)["left"], mesh1.dy) 
+        assert np.isclose(mesh1.getCellLengths(i)["right"] + mesh2.getCellLengths(i)["right"], mesh1.dy) 
+
+        assert np.isclose(mesh1.getCellLengths(i)["top"] + mesh2.getCellLengths(i)["top"], mesh1.dx) 
+        assert np.isclose(mesh1.getCellLengths(i)["bottom"] + mesh2.getCellLengths(i)["bottom"], mesh1.dx)
+
+    for i in insideNonConformalCells:
+        assert np.isclose(mesh1.getCellLengths(i)["left"] + mesh2.getCellLengths(i)["left"], mesh1.dy) 
+        assert np.isclose(mesh1.getCellLengths(i)["right"] + mesh2.getCellLengths(i)["right"], mesh1.dy) 
+
+        assert np.isclose(mesh1.getCellLengths(i)["top"] + mesh2.getCellLengths(i)["top"], mesh1.dx) 
+        assert np.isclose(mesh1.getCellLengths(i)["bottom"] + mesh2.getCellLengths(i)["bottom"], mesh1.dx) 
+
+    for i in outsideNonConformalCells:
+        assert np.isclose(mesh1.getCellLengths(i)["left"] + mesh2.getCellLengths(i)["left"], mesh1.dy) 
+        assert np.isclose(mesh1.getCellLengths(i)["right"] + mesh2.getCellLengths(i)["right"], mesh1.dy) 
+
+        assert np.isclose(mesh1.getCellLengths(i)["top"] + mesh2.getCellLengths(i)["top"], mesh1.dx) 
+        assert np.isclose(mesh1.getCellLengths(i)["bottom"] + mesh2.getCellLengths(i)["bottom"], mesh1.dx)  
 
 def test_cell_lengths_line():
     node_list = [(0,0), (10,1)]
