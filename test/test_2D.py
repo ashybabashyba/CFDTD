f0b04c91226f4822c55816a5f3de4427eecc4fe0
Carlos J. Ramos <cramoss@unal.edu.co>
2024-06-09 03:01:14 +0200

    Added visual test for rectangular resonant cavity for non conformal FDTD

--- test/test_2D.py
+++ test/test_2D.py
@@ -9,7 +9,7 @@ from initial_pulse.INITIAL_PULSE2D import *
 def test_visual_animation():
     node_list = [(25.5,10.5), (75.5,10.5), (75.5, 95.5), (25.5, 95.5)]
     initial_cell = (50, 50)
-    mesh = Mesh(box_size=100.0, dx=1.0, dy=2.0, external_nodes_list_PEC=node_list, initial_wave_cell=initial_cell)
+    mesh = Mesh(box_size=100.0, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list, initial_wave_cell=initial_cell)
     # mesh.plotElectricFieldGrid()
     pulse = InitialPulse(mesh=mesh, initial_position=initial_cell, spread=10.0, pulse_type='Magnetic Gaussian')
     solver = CFDTD2D(mesh, initialPulse=pulse, cfl=0.5)
@@ -18,13 +18,25 @@ def test_visual_animation():
 
     solver.plotMagneticFieldAnimation(nsteps)
 
-def test_rectangular_resonant_cavity11():
+def test_rectangular_resonant_cavity11_Conformal():
     node_list = [(10.5,10.5), (95.5,10.5), (95.5, 95.5), (10.5, 95.5)]
     initial_cell = (50, 50)
     mesh = Mesh(box_size=100.0, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list, initial_wave_cell=initial_cell)
     # mesh.plotElectricFieldGrid()
     pulse = InitialPulse(mesh=mesh, initial_position=initial_cell, spread=10.0, pulse_type='Rectangular Resonant Cavity 11')
-    solver = CFDTD2D(mesh, initialPulse=pulse, cfl=0.5)
+    solver = CFDTD2D(mesh, initialPulse=pulse, cfl=0.4)
+    nsteps = int(100 / solver.dt)
+    probeEx, probeEy, probeHz, probeTime = solver.run(nsteps)
+
+    solver.plotMagneticFieldAnimation(nsteps)
+
+def test_rectangular_resonant_cavity11_NonConformal():
+    node_list = [(10.5,10.5), (95.5,10.5), (95.5, 95.5), (10.5, 95.5)]
+    initial_cell = (50, 50)
+    mesh = Mesh(box_size=100.0, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list, initial_wave_cell=initial_cell)
+    # mesh.plotElectricFieldGrid()
+    pulse = InitialPulse(mesh=mesh, initial_position=initial_cell, spread=10.0, pulse_type='Non Conformal Rectangular Resonant Cavity 11')
+    solver = CFDTD2D(mesh, initialPulse=pulse, cfl=0.4, solver_type="Non Conformal")
     nsteps = int(100 / solver.dt)
     probeEx, probeEy, probeHz, probeTime = solver.run(nsteps)
 
