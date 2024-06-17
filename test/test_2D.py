4457f44db583f5d18fa1282cb4f324702d9570f7
Carlos J. Ramos <cramoss@unal.edu.co>
2024-06-17 21:49:21 +0200

    Fixed the test of the Gaussian in one line

--- test/test_2D.py
+++ test/test_2D.py
@@ -18,7 +18,7 @@ def test_visual_animation():
 
     solver.plotMagneticFieldAnimation(nsteps)
 
-def test_rectangular_resonant_cavity11_Conformal():
+def test_rectangular_resonant_cavity44_Conformal():
     node_list = [(10.5,10.5), (95.5,10.5), (95.5, 95.5), (10.5, 95.5)]
     initial_cell = (50, 50)
     mesh = Mesh(box_size=100.0, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list, initial_wave_cell=initial_cell)
@@ -30,7 +30,7 @@ def test_rectangular_resonant_cavity11_Conformal():
 
     solver.plotMagneticFieldAnimation(nsteps)
 
-def test_rectangular_resonant_cavity11_NonConformal():
+def test_rectangular_resonant_cavity44_NonConformal():
     node_list = [(10.5,10.5), (95.5,10.5), (95.5, 95.5), (10.5, 95.5)]
     initial_cell = (50, 50)
     mesh = Mesh(box_size=100.0, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list, initial_wave_cell=initial_cell)
@@ -42,17 +42,17 @@ def test_rectangular_resonant_cavity11_NonConformal():
 
     solver.plotMagneticFieldAnimation(nsteps)
 
-def test_Ey_Gaussian():
-    node_list = [(5.5,25.5), (95.5,25.5), (95.5, 75.5), (5.5, 75.5)]
+def test_Hy_Gaussian():
+    node_list = [(5.5,40.5), (94.5,40.5), (94.5, 60.5), (5.5, 60.5)]
     initial_cell = (50, 50)
-    mesh = Mesh(box_size=100.0, dx=1.0, dy=1.0, external_nodes_list_PEC=node_list, initial_wave_cell=initial_cell)
+    mesh = Mesh(box_size=100.0, dx=2.0, dy=2.0, external_nodes_list_PEC=node_list, initial_wave_cell=initial_cell)
     # mesh.plotElectricFieldGrid()
-    pulse = InitialPulse(mesh=mesh, initial_position=initial_cell, spread=1, pulse_type='ElectricY Gaussian')
+    pulse = InitialPulse(mesh=mesh, initial_position=initial_cell, spread=10.0, pulse_type='MagneticY Gaussian')
     solver = CFDTD2D(mesh, initialPulse=pulse, cfl=0.4)
     nsteps = int(100 / solver.dt)
     probeEx, probeEy, probeHz, probeTime = solver.run(nsteps)
 
-    # solver.plotMagneticFieldAnimation(nsteps)
+    solver.plotMagneticFieldAnimation(nsteps)
     # for i in range(nsteps):
     #     plt.plot(mesh.gridEy, probeEy[int(mesh.gridEx[-1]/2), :, i], '.-', label='Electric Field')
     #     plt.ylim(-2.1, 2.1)
