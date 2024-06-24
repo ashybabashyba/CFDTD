53b0cbb27f4d62b56d4faed60129d2c08610f8b8
Carlos J. Ramos <cramoss@unal.edu.co>
2024-06-24 21:45:50 +0200

    Fixxed initial pulse for the non conformal case

--- initial_pulse/INITIAL_PULSE2D.py
+++ initial_pulse/INITIAL_PULSE2D.py
@@ -19,9 +19,9 @@ class InitialPulse():
     def buildPulse(self):
         if self.type == "Magnetic Gaussian":
             self.Hz = magneticGaussian(H0=self.Hz, mesh=self.mesh, center=self.center, spread=self.spread)
-        elif self.type == "Rectangular Resonant Cavity 11":
+        elif self.type == "Rectangular Resonant Cavity":
             self.Hz = rectangularResonantCavity(H0=self.Hz, mesh=self.mesh, m=self.m, n=self.n)
-        elif self.type == "Non Conformal Rectangular Resonant Cavity 11":
+        elif self.type == "Non Conformal Rectangular Resonant Cavity":
             self.Hz = rectangularResonantCavityNonConformal(H0=self.Hz, mesh=self.mesh, m=self.m, n=self.n)
         elif self.type == "MagneticY Gaussian":
             self.Hz = electricYGaussian(H0=self.Hz, mesh=self.mesh, center=self.center, spread=self.spread)
@@ -54,12 +54,19 @@ def rectangularResonantCavityNonConformal(H0, mesh, m, n):
     conformalCells, outsideNonConformalCells, insideNonConformalCells = mesh.getCellSeparationByType()
     a = np.abs(mesh.nodesList[0][0] - mesh.nodesList[1][0])
     b = np.abs(mesh.nodesList[0][1] - mesh.nodesList[3][1])
+
+    xmin, xmax, ymin, ymax = mesh.getMinMaxIndexInsideNonConformalCells()
+    dx_l = mesh.getCellLengths((xmin-1, ymin))["bottom"]
+    dx_r = mesh.getCellLengths((xmax+1, ymin))["bottom"]
+    dy_b = mesh.getCellLengths((xmin, ymin-1))["left"]
+    dy_t = mesh.getCellLengths((xmin, ymax+1))["left"]
+
     x0 = mesh.nodesList[0][0]
     y0 = mesh.nodesList[0][1]
     for i in range(mesh.gridHx.size):
         for j in range(mesh.gridHy.size):
             if (i,j) in insideNonConformalCells:
-                initialH[i,j] = np.cos(m*np.pi*(mesh.gridHx[i] - x0 + mesh.dx/2)/(a-mesh.dx))*np.cos(n*np.pi*(mesh.gridHy[j] - y0 + mesh.dy/2)/(b-mesh.dy))
+                initialH[i,j] = np.cos(m*np.pi*(mesh.gridHx[i] - x0 - dx_l)/(a-dx_l-dx_r))*np.cos(n*np.pi*(mesh.gridHy[j] - y0 - dy_b)/(b-dy_b-dy_t))
     return initialH
 
 def electricYGaussian(H0, mesh, center, spread):
