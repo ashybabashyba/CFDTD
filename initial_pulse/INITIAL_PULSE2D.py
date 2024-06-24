46945d2a99ae88936bae469c4e28840a53c39b13
Carlos J. Ramos <cramoss@unal.edu.co>
2024-06-24 10:51:22 +0200

    Generalized the rectangular resonant cavity to insert any mode

--- initial_pulse/INITIAL_PULSE2D.py
+++ initial_pulse/INITIAL_PULSE2D.py
@@ -3,11 +3,14 @@ import math
 from matplotlib import pyplot as plt
 
 class InitialPulse():
-    def __init__(self, mesh, initial_position, spread, pulse_type):
+    def __init__(self, mesh, initial_position, spread, pulse_type, m=None, n=None):
         self.center = initial_position
         self.spread = spread
         self.type = pulse_type
         self.mesh = mesh
+
+        self.m = m
+        self.n = n
 
         self.Ex = np.zeros((self.mesh.gridEx.size, self.mesh.gridEy.size))
         self.Ey = np.zeros((self.mesh.gridEx.size, self.mesh.gridEy.size))
@@ -17,9 +20,9 @@ class InitialPulse():
         if self.type == "Magnetic Gaussian":
             self.Hz = magneticGaussian(H0=self.Hz, mesh=self.mesh, center=self.center, spread=self.spread)
         elif self.type == "Rectangular Resonant Cavity 11":
-            self.Hz = rectangularResonantCavity11(H0=self.Hz, mesh=self.mesh)
+            self.Hz = rectangularResonantCavity(H0=self.Hz, mesh=self.mesh, m=self.m, n=self.n)
         elif self.type == "Non Conformal Rectangular Resonant Cavity 11":
-            self.Hz = rectangularResonantCavity11NonConformal(H0=self.Hz, mesh=self.mesh)
+            self.Hz = rectangularResonantCavityNonConformal(H0=self.Hz, mesh=self.mesh, m=self.m, n=self.n)
         elif self.type == "MagneticY Gaussian":
             self.Hz = electricYGaussian(H0=self.Hz, mesh=self.mesh, center=self.center, spread=self.spread)
         else:
@@ -33,7 +36,7 @@ def magneticGaussian(H0, mesh, center, spread):
             initialH[i,j] = math.exp(- ((mesh.gridHx[i]-center[0])**2 + (mesh.gridHy[j]-center[1])**2) /math.sqrt(2.0) / spread)     
     return initialH
 
-def rectangularResonantCavity11(H0, mesh):
+def rectangularResonantCavity(H0, mesh, m, n):
     initialH = np.zeros(H0.shape)
     conformalCells, outsideNonConformalCells, insideNonConformalCells = mesh.getCellSeparationByType()
     a = np.abs(mesh.nodesList[0][0] - mesh.nodesList[1][0])
@@ -43,10 +46,10 @@ def rectangularResonantCavity11(H0, mesh):
     for i in range(mesh.gridHx.size):
         for j in range(mesh.gridHy.size):
             if (i,j) not in outsideNonConformalCells:
-                initialH[i,j] = np.cos(4*np.pi*(mesh.gridHx[i] - x0)/a)*np.cos(4*np.pi*(mesh.gridHy[j] - y0)/b)
+                initialH[i,j] = np.cos(m*np.pi*(mesh.gridHx[i] - x0)/a)*np.cos(n*np.pi*(mesh.gridHy[j] - y0)/b)
     return initialH
 
-def rectangularResonantCavity11NonConformal(H0, mesh):
+def rectangularResonantCavityNonConformal(H0, mesh):
     initialH = np.zeros(H0.shape)
     conformalCells, outsideNonConformalCells, insideNonConformalCells = mesh.getCellSeparationByType()
     a = np.abs(mesh.nodesList[0][0] - mesh.nodesList[1][0])
@@ -56,7 +59,7 @@ def rectangularResonantCavity11NonConformal(H0, mesh):
     for i in range(mesh.gridHx.size):
         for j in range(mesh.gridHy.size):
             if (i,j) in insideNonConformalCells:
-                initialH[i,j] = np.cos(4*np.pi*(mesh.gridHx[i] - x0 + mesh.dx/2)/(a-mesh.dx))*np.cos(4*np.pi*(mesh.gridHy[j] - y0 + mesh.dy/2)/(b-mesh.dy))
+                initialH[i,j] = np.cos(m*np.pi*(mesh.gridHx[i] - x0 + mesh.dx/2)/(a-mesh.dx))*np.cos(n*np.pi*(mesh.gridHy[j] - y0 + mesh.dy/2)/(b-mesh.dy))
     return initialH
 
 def electricYGaussian(H0, mesh, center, spread):
